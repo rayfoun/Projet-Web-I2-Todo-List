851c5d8120a8c57e205f47b649fa53bfc77d627b
Esmeralda Kameni <kamenia@3il.fr>
2025-01-16 12:29:21 +0100

    dao1

--- Modele/entite/Utilisateur.php
+++ Modele/entite/Utilisateur.php
@@ -7,19 +7,17 @@ class Utilisateur {
     public $password_utilisateur;
     public $email_utilisateur;
     public $token_utilisateur;
-    public $login_utilisateur;
     public $type;
     public $photo_utilisateur;
     public $verification_email_utilisateur;
 
-    public function __construct($id_utilisateur, $nom_utilisateur, $prenom_utilisateur, $password_utilisateur, $email_utilisateur, $token_utilisateur, $login_utilisateur, $type, $photo_utilisateur, $verification_email_utilisateur) {
+    public function __construct($id_utilisateur, $nom_utilisateur, $prenom_utilisateur, $password_utilisateur, $email_utilisateur, $token_utilisateur, $type, $photo_utilisateur, $verification_email_utilisateur) {
         $this->id_utilisateur = $id_utilisateur;
         $this->nom_utilisateur = $nom_utilisateur;
         $this->prenom_utilisateur = $prenom_utilisateur;
         $this->password_utilisateur = $password_utilisateur;
         $this->email_utilisateur = $email_utilisateur;
         $this->token_utilisateur = $token_utilisateur;
-        $this->login_utilisateur = $login_utilisateur;
         $this->type = $type;
         $this->photo_utilisateur = $photo_utilisateur;
         $this->verification_email_utilisateur = $verification_email_utilisateur;

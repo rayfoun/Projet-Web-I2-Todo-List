728501f3ca951311a6cf0f15ad0c603e30dc7239
Rayan <rayanfounsie@gmail.com>
2025-01-20 11:51:36 +0100

    Connexion et authentification

--- controlleur/ConnexionController.php
+++ controlleur/ConnexionController.php
@@ -22,7 +22,7 @@
             $email = $_POST["email"];
             $password = $_POST["password"];
     
-            $textR = "select password_utser, type ";
+            $textR = "select password_utser, type, id_user ";
             $textR.= "from users ";
             $textR.= "where email_user=:email";
             $req = $pdo->prepare($textR);
@@ -34,28 +34,39 @@
             $tabRes = $req->fetchAll(PDO::FETCH_ASSOC);
             $var=count($tabRes);
             echo $var;
+            if ($tabRes != null){
+                var_dump($tabRes[0]);
+                $hashedPassword = password_hash($tabRes[0]['password_utser'], PASSWORD_DEFAULT);
+                echo $hashedPassword;
+            }
+            
+            
             //Vérification de l'existence de l'utilisateur
             if (count($tabRes)!=1) {
                 // pas trouvé => retour au formulaire de co
                 // die("Erreur de co");
-               // header("Location: /../Projet-Web-I2-Todo-List/Routeur/routeur.php");
-                //exit();
+                header("Location: /../Projet-Web-I2-Todo-List/Routeur/routeur.php");
+                echo "Nom d'utilisateur incorrect";
+                exit();
             }    
             
             //Vérification du mot de passe crypté
-            if (!password_verify($password, $tabRes[0]['password'])) {
+            if (!password_verify($password, $hashedPassword)) {
                 // Mot de passe incorrect
-                //header("Location: /../Projet-Web-I2-Todo-List/Routeur/routeur.php");
+                header("Location: /../Projet-Web-I2-Todo-List/Routeur/routeur.php");
+                echo "Mot de passe incorrect";
                 exit();
             }
+            echo "Tres bonne connexion amigo";
 
             // Authentification réussie
             //Enregistrement des informations dans la session
-            $_SESSION["email"] = $email;  
+            $_SESSION["id_user"] = $tabRes[0]["id_user"];  
             $_SESSION["type"] = $tabRes[0]["type"];
+
             
             // Redirection vers la page d'accueil
-            //header("Location:/../Projet-Web-I2-Todo-List/Routeur/routeur.php?action=accueil");
+            header("Location:/../Projet-Web-I2-Todo-List/Routeur/routeur.php?action=accueil");
         }
     }
 ?>

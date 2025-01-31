90f2441e27a6aee4aaf23f136206a1d5b23c1869
BAYIHA GUETH Mydrane Daphné <bayihaguethdaphne@gmail.com>
2025-01-31 14:50:44 +0100

    conflic resolu

--- controlleur/controllerAccueil.php
+++ controlleur/controllerAccueil.php
@@ -364,13 +364,9 @@ class ControllerAccueil extends DefaultController {
         exit;
     }
 
-<<<<<<< HEAD
 
     public function searchForm(){
-=======
-    public function searchForm(){
 
->>>>>>> 9589c0d5e9f1aa4dcc6a3742407bb29445bc6ba0
         if (isset($_GET['action']) && $_GET['action'] === 'searchList') {
             // Récupérer les paramètres de recherche depuis la requête GET
             $libelle = $_GET['libelle'] ?? null;
@@ -390,13 +386,8 @@ class ControllerAccueil extends DefaultController {
             // Effectuer la recherche
             $tacheDao = new TacheDao();
 
-<<<<<<< HEAD
             $listeTaches = $tacheDao->getTasksByFilters($libelle, $statut, $priorite, $utilisateurId, $categorie);
-=======
-            $taches = $tacheDao->getTasksByFilters($libelle, $statut, $priorite, $utilisateurId, $categorie);
->>>>>>> 9589c0d5e9f1aa4dcc6a3742407bb29445bc6ba0
 
-    
             $listeTache=$this->formatedListTask($listeTaches);
             // Créer la réponse en format JSON
                $response = [
